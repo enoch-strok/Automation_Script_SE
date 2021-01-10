8f8edecc1ed0b6b36a3496b395dfe112de5c299f
Enoch <enochstrok@outlook.com>
2021-01-09 19:45:49 -0800

    Added 2 Complete Scripts

---
+++ Nexus_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        // to learn more about ingame scripts.
+        // Go to:
+        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
+    
+
+        IMyLightingBlock light;
+
+        public Program()
+        {
+
+            light = GridTerminalSystem.GetBlockGroupWithName("lights") as IMyLightingBlock;
+        }
+
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+
+            light.Enabled = false;
+
+        }
+    }
+}
